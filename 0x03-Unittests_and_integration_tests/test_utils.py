a57b1c12a575bd2015d6fe9e3c445a76d621861d
Ayobami <oludemiayobami@gmail.com>
2024-05-14 04:47:58 +0100

    feat: task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """0. Parameterize a unit test"""
 import unittest
+from unittest import mock
 from parameterized import parameterized
 
 import utils
@@ -19,10 +20,26 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(utils.access_nested_map(access_map, path), expected)
 
     @parameterized.expand([
-        ({"a": 1}, ("a",)),
+        ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, access_map, path):
         """Tests that an assertion is raised"""
         with self.assertRaises(KeyError):
             utils.access_nested_map(access_map, path)
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @mock.patch("utils.requests.get")
+    def test_get_json(self, url, payload, get):
+        """Test get_json function"""
+        json_mock = mock.Mock()
+        json_mock.json.return_value = payload
+        get.return_value = json_mock
+
+        res = utils.get_json(url)
+
+        get.assert_called_once_with(url)
+        self.assertEqual(res, payload)
